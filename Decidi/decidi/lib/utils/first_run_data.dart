f874493244fc1da87be7bc93bb17a08d84b0d5e9
med wajdi brahem <wajdibrahem74@gmail.com>
2022-02-23 13:03:01 +0100

    firstRun Screen

---
+++ Decidi/decidi/lib/utils/first_run_data.dart
@@ -0,0 +1,28 @@
+List<String> InfoMat = [
+  "Algorithmique",
+  "Mathematique",
+  "Bases de donnees",
+  "Physique",
+  "Anglais",
+  "Francais",
+  "Arabe",
+  "TIC",
+  "Philo",
+  "Sport",
+  "Option",
+  "Moy Generale"
+];
+
+List<String> ScienceMat = [
+  "Physique",
+  "Science",
+  "Mathematique",
+  "Anglais",
+  "Francais",
+  "Arabe",
+  "Philo",
+  "Info",
+  "Sport",
+  "Option",
+  "Moy Generale"
+];
