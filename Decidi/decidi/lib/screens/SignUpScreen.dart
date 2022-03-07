7849b67884e7c701c6e4ebc3c00f2f94cb478075
TirariAchref <79058936+TirariAchref@users.noreply.github.com>
2022-03-07 11:33:43 +0100

    ag

--- Decidi/decidi/lib/screens/SignUpScreen.dart
+++ Decidi/decidi/lib/screens/SignUpScreen.dart
@@ -5,10 +5,14 @@ import 'package:decidi/screens/SignInScreen.dart';
 import 'package:decidi/screens/first_run/first_run.dart';
 import 'package:decidi/screens/root_app.dart';
 import 'package:decidi/utils/constant.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:http/http.dart' as http;
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:flutter_facebook_auth/flutter_facebook_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 
 class SignUpScreen extends StatefulWidget {
   @override
@@ -57,6 +61,22 @@ class _SignUpScreenState extends State<SignUpScreen> {
     emailAddressController = TextEditingController();
     passwordController = TextEditingController();
     passwordVisibility = false;
+  }
+
+  late GoogleSignInAccount? googleuserr;
+
+  GoogleSignIn _googleSignIn = GoogleSignIn();
+  Future<UserCredential> signInWithGoogle() async {
+    final GoogleSignInAccount? googleuser = await GoogleSignIn().signIn();
+
+    final GoogleSignInAuthentication googleAuth =
+        await googleuser!.authentication;
+    googleuserr = googleuser;
+    final OAuthCredential credential = GoogleAuthProvider.credential(
+        idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+    Fluttertoast.showToast(msg: "Account created");
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   @override
@@ -607,8 +627,46 @@ class _SignUpScreenState extends State<SignUpScreen> {
                                   color: Color(0xFF57636C),
                                   size: 20,
                                 ),
-                                onPressed: () {
-                                  print('IconButton pressed ...');
+                                onPressed: () async {
+                                  signInWithGoogle();
+                                  print(
+                                      "///////////////////////////////////////////////");
+                                  print(googleuserr!.email);
+
+                                  Map<String, dynamic> userData = {
+                                    "firstName": googleuserr!.displayName,
+                                    "email": googleuserr!.email,
+                                    "password": "password",
+                                    // "role": "client"
+                                  };
+                                  print(userData);
+                                  Map<String, String> headers = {
+                                    "Content-Type":
+                                        "application/json; charset=UTF-8"
+                                  };
+                                  http
+                                      .post(Uri.http(baseUrl, "/createuser"),
+                                          headers: headers,
+                                          body: json.encode(userData))
+                                      .then((http.Response response) async {
+                                    if (response.statusCode == 200) {
+                                      Map<String, dynamic> userData =
+                                          json.decode(response.body);
+
+                                      // SharedPreferences
+                                      SharedPreferences prefs =
+                                          await SharedPreferences.getInstance();
+                                      prefs.setString(
+                                          "userId", userData["_id"]);
+
+                                      Navigator.of(context).pushReplacement(
+                                        MaterialPageRoute<void>(
+                                          builder: (BuildContext context) =>
+                                              RootApp(),
+                                        ),
+                                      );
+                                    }
+                                  });
                                 },
                               ),
                               IconButton(
@@ -629,8 +687,65 @@ class _SignUpScreenState extends State<SignUpScreen> {
                                   color: Color(0xFF57636C),
                                   size: 20,
                                 ),
-                                onPressed: () {
-                                  print('IconButton pressed ...');
+                                onPressed: () async {
+                                  final LoginResult result = await FacebookAuth
+                                      .instance
+                                      .login(permissions: [
+                                    "email",
+                                    "public_profile"
+                                  ]);
+                                  if (result.status == LoginStatus.success) {
+                                    // you are logged
+
+                                    final AccessToken accessToken =
+                                        result.accessToken!;
+                                    String url =
+                                        "https://graph.facebook.com/v2.12/me?fields=name,first_name,last_name,email&access_token=${accessToken.token}";
+                                    Uri myUri = Uri.parse(url);
+                                    var graphResponse = await http.get(myUri);
+
+                                    var profile =
+                                        json.decode(graphResponse.body);
+                                    Map<String, dynamic> userData = {
+                                      "firstName": profile["first_name"],
+                                      "lastName": profile["last_name"],
+                                      "email": profile["email"],
+                                      "password": "password",
+                                      // "role": "client"
+                                    };
+                                    print(userData);
+                                    Map<String, String> headers = {
+                                      "Content-Type":
+                                          "application/json; charset=UTF-8"
+                                    };
+                                    http
+                                        .post(Uri.http(baseUrl, "/createuser"),
+                                            headers: headers,
+                                            body: json.encode(userData))
+                                        .then((http.Response response) async {
+                                      if (response.statusCode == 200) {
+                                        Map<String, dynamic> userData =
+                                            json.decode(response.body);
+
+                                        // SharedPreferences
+                                        SharedPreferences prefs =
+                                            await SharedPreferences
+                                                .getInstance();
+                                        prefs.setString(
+                                            "userId", userData["_id"]);
+
+                                        Navigator.of(context).pushReplacement(
+                                          MaterialPageRoute<void>(
+                                            builder: (BuildContext context) =>
+                                                RootApp(),
+                                          ),
+                                        );
+                                      }
+                                    });
+                                  } else {
+                                    print(result.status);
+                                    print(result.message);
+                                  }
                                 },
                               ),
                             ],
