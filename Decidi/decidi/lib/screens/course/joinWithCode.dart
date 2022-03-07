106d46d1af5ac030276e2effd8d64538f9b3fdeb
TirariAchref <79058936+TirariAchref@users.noreply.github.com>
2022-03-07 20:55:15 +0100

    3 in 1

--- Decidi/decidi/lib/screens/course/joinWithCode.dart
+++ Decidi/decidi/lib/screens/course/joinWithCode.dart
@@ -1,5 +1,6 @@
 import 'package:agora_rtc_engine/rtc_engine.dart';
 import 'package:decidi/screens/course/video_call.dart';
+import 'package:decidi/theme/color.dart';
 import 'package:flutter/material.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -17,7 +18,7 @@ class JoinWithCode extends StatelessWidget {
             Align(
               alignment: Alignment.topLeft,
               child: InkWell(
-                  child: Icon(Icons.arrow_back_ios_new_sharp, size: 35),
+                  child: Icon(Icons.arrow_back_outlined, size: 35),
                   onTap: () {
                     Navigator.pop(context);
                   }),
@@ -31,14 +32,14 @@ class JoinWithCode extends StatelessWidget {
             SizedBox(height: 20),
             Text(
               "Enter meeting code below",
-              style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+              style: TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
             ),
             Padding(
               padding: const EdgeInsets.fromLTRB(15, 20, 15, 20),
               child: Card(
                 color: Colors.grey[300],
                 shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(25),
+                  borderRadius: BorderRadius.circular(15),
                 ),
                 child: TextField(
                   controller: _controller,
@@ -49,41 +50,46 @@ class JoinWithCode extends StatelessWidget {
                 ),
               ),
             ),
-            ElevatedButton(
-              onPressed: () async {
-                // update input validation
-                // await for camera and mic permissions before pushing video page
-                await _handleCameraAndMic(Permission.camera);
-                await _handleCameraAndMic(Permission.microphone);
-                // push video page with given channel name
-                if (_controller.text.trim().isEmpty) {
-                  print("empty");
-                } else {
-                  print(_controller.text.trim());
-                  await Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => CallPage(
-                        channelName: _controller.text.trim(),
-                        role: _role,
+            SizedBox(
+              height: 45,
+              width: MediaQuery.of(context).size.height * 0.55,
+              child: ElevatedButton(
+                onPressed: () async {
+                  // update input validation
+                  // await for camera and mic permissions before pushing video page
+                  await _handleCameraAndMic(Permission.camera);
+                  await _handleCameraAndMic(Permission.microphone);
+                  // push video page with given channel name
+                  if (_controller.text.trim().isEmpty) {
+                    print("empty");
+                  } else {
+                    print(_controller.text.trim());
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => CallPage(
+                          channelName: _controller.text.trim(),
+                          role: _role,
+                        ),
                       ),
-                    ),
-                  );
-                }
-                /*Navigator.of(context).push(
+                    );
+                  }
+                  /*Navigator.of(context).push(
                   MaterialPageRoute<void>(
                     builder: (BuildContext context) =>
                         VideoCall(channelName: _controller.text.trim()),
                   ),
                 );*/
-                //Get.to(VideoCall(channelName: _controller.text.trim()));
-              },
-              child: Text("Join"),
-              style: ElevatedButton.styleFrom(
-                fixedSize: Size(50, 30),
-                primary: Colors.indigo,
-                shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(25)),
+                  //Get.to(VideoCall(channelName: _controller.text.trim()));
+                },
+                child: Text("Join"),
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  primary: primary,
+                  shadowColor: shadowColor,
+                  elevation: 1.0,
+                ),
               ),
             ),
           ],
