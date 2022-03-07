4bc0ccf072cb23ff2fbe9fccb24283479e1e711c
TirariAchref <79058936+TirariAchref@users.noreply.github.com>
2022-03-07 12:50:37 +0100

    final

--- Decidi/decidi/lib/main.dart
+++ Decidi/decidi/lib/main.dart
@@ -27,7 +27,7 @@ class MyApp extends StatelessWidget {
           theme: ThemeData(
             primaryColor: primary,
           ),
-          home: DashboardAdmin()
+          home: SignInScreen()
           //RootApp()
           // HomePage()
 
