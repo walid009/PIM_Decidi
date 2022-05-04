18f0051b3b35879f0a31f9435636f2001ca0eb47
Mohamed Amine Bellili <73409316+medAmineBell@users.noreply.github.com>
2022-05-04 14:18:26 +0100

    update

--- Decidi/decidi/lib/screens/first_run/display_my_text_Eco.dart
+++ Decidi/decidi/lib/screens/first_run/display_my_text_Eco.dart
@@ -1,11 +1,10 @@
 import 'dart:convert';
-import 'package:cool_alert/cool_alert.dart';
 import 'package:decidi/providers/DataProvider.dart';
 import 'package:decidi/screens/SignInScreen.dart';
 import 'package:decidi/theme/color.dart';
 import 'package:decidi/utils/first_run_data.dart';
 import 'first_run.dart';
-import 'package:decidi/screens/root_app.dart';
+
 import 'package:decidi/utils/constant.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
@@ -54,18 +53,17 @@ class _DisplayMyTextEcoState extends State<DisplayMyTextEco> {
       option.text = widget.bacGradesData[10].x;
       moy.text = widget.bacGradesData[11].x;
     } else {
-      openAlert("Error", "Bac Type is worng or Image not clear!");
+      WidgetsBinding.instance?.addPostFrameCallback((_) async {
+        showDialog(
+            context: context,
+            builder: (BuildContext context) {
+              return AlertDialog(
+                title: Text("Error"),
+                content: Text("Bac Type is worng or Image not clear!"),
+              );
+            });
+      });
     }
-  }
-
-  // Advanced using of alerts
-  Future<void> openAlert(String title, String desc) async {
-    await CoolAlert.show(
-        context: context,
-        type: CoolAlertType.warning,
-        title: title,
-        text: desc,
-        backgroundColor: Theme.of(context).primaryColor);
   }
 
   @override
